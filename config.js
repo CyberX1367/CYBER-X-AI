d3f2cae17c366cce93d65b246939cd94d57041cf
CyberX1367 <cyberx.1367@gmail.com>
2025-04-19 19:18:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "WNtTQJxB#q4fNOED2bY4m5kbolgqMDtzl654SU-kmTPk2hmsno8k",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:jBkYtZASwDDwTgargOLGsMQuwHtwRGLh@interchange.proxy.rlwy.net:47569",
+  OWNER_NUM: process.env.OWNER_NUM || "94750915633",
 };
