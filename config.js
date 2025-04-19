eca19394e403141e598c235be767afb7ceb8e200
CyberX1367 <cyberx.1367@gmail.com>
2025-04-19 22:10:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,8 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "WNtTQJxB#q4fNOED2bY4m5kbolgqMDtzl654SU-kmTPk2hmsno8k",
   OWNER_NUM: process.env.OWNER_NUM || "94750915633",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/CyberX1367/Bot-Media/refs/heads/main/alive-img.jpeg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n😎𝐌𝐚𝐝𝐞 𝐛𝐲 C_Y_B_E_R😎",
+  AUTO_READ_STATUS: process.env. AUTO_READ_STATUS || "false",
+  MODE: process.env.MODE || "private",
 };
