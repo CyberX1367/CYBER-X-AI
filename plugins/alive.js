2044b577804ae8dd204abecde3ef5eec0c618f59
CyberX1367 <cyberx.1367@gmail.com>
2025-04-20 14:46:41 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     await robin.sendPresenceUpdate('recording', from);
-    await robin.sendMessage(from, { audio: { url: "https://github.com/CyberX1367/Bot-Media/blob/main/audio/alive.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+    await robin.sendMessage(from, { audio: { url: "https://github.com/CyberX1367/Bot-Media/raw/refs/heads/main/audio/alive.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
     await robin.sendMessage(from,{sticker: { url : "https://raw.githubusercontent.com/CyberX1367/Bot-Media/refs/heads/main/sticker/alive-sticker.jpg" },package: 'C_Y_B_E_R'},{ quoted: mek }) 
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
